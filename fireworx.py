3455cf7ed9536c5ba5437d5d7f857eb72373c25d
hemidactylus <hemidactylus@users.noreply.github.com>
2026-05-02 23:12:13 +0200

    more fireworx

--- fireworx.py
+++ fireworx.py
@@ -8,9 +8,11 @@ from egame.geometry import Scaler
 from egame.things import PhysicsThing
 from egame.type_definitions import FPair
 
-MAX_GEN = 2
-NUM_FRAGMENTS = 10
-DV_MOD = 3.0
+MAX_GEN = 5
+NUM_FRAGMENTS = 20
+DV_MOD = 12.0
+GEN_LC_BASE = 25
+GEN_LC_DELTA = 3
 
 LSIZE = (16, 10)
 SIZE = (1600, 1000)
@@ -21,6 +23,7 @@ COLORS = [
     (255, 0, 0, 255),
     (255, 255, 0, 255),
     (0, 255, 255, 255),
+    (100, 0, 215, 255),
 ]
 
 
@@ -63,7 +66,7 @@ class Exploder(PhysicsThing):
                     lc0 = None
                 else:
                     # TODO debug timescale, it's not s?
-                    lc0 = 80 - self.gen * 10
+                    lc0 = GEN_LC_BASE - self.gen * GEN_LC_DELTA
                 for i in range(NUM_FRAGMENTS):
                     new_v = (
                         self.lv[0] + DV_MOD * (2 * random.random() - 1),
@@ -84,19 +87,19 @@ class Exploder(PhysicsThing):
 
 
 if __name__ == "__main__":
-    ctx0 = Context(size=SIZE, lsize=LSIZE, lg=(0.0, -1.0), time_factor=1)
+    ctx0 = Context(size=SIZE, lsize=LSIZE, lg=(0.0, -10.0), time_factor=1.0)
 
     def on_k_p(ctx: Context, symbol: int, modifiers: int) -> Literal[True] | None:
         if symbol == pyglet.window.key.SPACE:
             x0 = random.random() * LSIZE[0]
-            vx = (4 * random.random() - 1) * 1.0
-            vy = 3.5 + 1.0 * (2 * random.random() - 1.0)
+            vx = (2 * random.random() - 1) * 7.0
+            vy = 10 + 2.0 * (2 * random.random() - 1.0)
 
             e0 = Exploder(
                 lpos=(x0, 0),
                 lv=(vx, vy),
                 gen=0,
-                lifecycle_s=80,
+                lifecycle_s=GEN_LC_BASE if MAX_GEN > 0 else None,
                 scaler=ctx0.scaler,
             )
             ctx.push_thing(e0)
--- game0.py
+++ game0.py
@@ -1,3 +1,5 @@
+import random  # TODO rnd lib
+
 from typing import Literal
 
 import pyglet
@@ -12,8 +14,8 @@ class Boo(Thing):
     def __init__(self, scaler: Scaler) -> None:
         car_img = pyglet.image.load("car.png")
         car_sprite = pyglet.sprite.Sprite(car_img, x=0, y=0)
-        car_sprite.scale_x = scaler.r_x(3) / car_img.width
-        car_sprite.scale_y = scaler.r_y(3) / car_img.height
+        car_sprite.scale_x = scaler.r_x(2) / car_img.width
+        car_sprite.scale_y = scaler.r_y(1.4) / car_img.height
         Thing.__init__(
             self,
             lpos=(5, 5),
@@ -28,7 +30,7 @@ class Boo(Thing):
 
 class Baa(PhysicsThing):
     def __init__(self, lpos: FPair, scaler: Scaler) -> None:
-        ball = pyglet.shapes.Circle(0, 0, scaler.r_x(0.3), color=(0, 255, 0, 255))
+        ball = pyglet.shapes.Circle(0, 0, scaler.r_x(0.3), color=(90, 47, 7, 255))
         PhysicsThing.__init__(
             self,
             lpos=lpos,
@@ -43,17 +45,48 @@ class Baa(PhysicsThing):
 
     def dies_on_update(self, ctx: "Context", dt: float, t_s: float) -> bool:
         dying = PhysicsThing.dies_on_update(self, ctx=ctx, dt=dt, t_s=t_s)
-        if dying:
-            if self.lpos[1] < 0:
-                ctx.push_thing(Baa((8, 8.5), ctx.scaler))
+        return dying
+
+
+class Food(Thing):
+    terminating: bool
+
+    def __init__(self, lpos: FPair, scaler: Scaler) -> None:
+        square = pyglet.shapes.Rectangle(0, 0, scaler.r_x(0.3), scaler.r_x(0.3), color=(44, 210, 130, 255))
+        self.terminating = False
+        Thing.__init__(
+            self,
+            lpos=lpos,
+            lsize=(0.3, 0.3),
+            sprites={"0": square},
+            sprite_offsets={"0": (0, 0)},
+            t0_s=0.0,
+            scaler=scaler,
+        )
+
+    def terminate(self) -> None:
+        self.terminating = True
+
+    def dies_on_update(self, ctx: "Context", dt: float, t_s: float) -> bool:
+        if self.terminating:
+            return True
+        dying = Thing.dies_on_update(self, ctx=ctx, dt=dt, t_s=t_s)
         return dying
 
 
 if __name__ == "__main__":
-    ctx0 = Context(size=(400, 400), lsize=(10.0, 10.0), lg=(0.0, -1.0), time_factor=4.0)
+    ctx0 = Context(size=(1200, 1200), lsize=(10.0, 10.0), lg=(0.0, -1.0), time_factor=4.0)
 
     boo = Boo(ctx0.scaler)
+    foo = Food(
+        (
+            random.random() * 10.0,
+            random.random() * 10.0,
+        ),
+        ctx0.scaler,
+    )
     ctx0.push_thing(boo)
+    ctx0.push_thing(foo)
 
     def on_k_p(ctx: Context, symbol: int, modifiers: int) -> Literal[True] | None:
         ball_n_pos = boo.lpos
@@ -67,7 +100,22 @@ if __name__ == "__main__":
             ball_n_pos = (ball_n_pos[0] + 0.5, ball_n_pos[1])
         elif symbol == pyglet.window.key.A:
             ctx.push_thing(Baa(boo.lpos, ctx.scaler))
+
         if ball_n_pos != boo.lpos:
+            # has eaten food?
+            foo_delta = (
+                abs(foo.lpos[0] + foo.sprite_offsets["0"][0] - boo.lpos[0]),
+                abs(foo.lpos[1] + foo.sprite_offsets["0"][1] - boo.lpos[1]),
+            )
+            # TODO bug with position (-> expose center position on a main sprite?)
+            if foo_delta[0] <= 0.7 and foo_delta[1] <= 1.2:
+                ctx.push_thing(Baa(boo.lpos, ctx.scaler))
+                new_foo_lpos = (
+                    random.random() * 10.0,
+                    random.random() * 10.0,
+                )
+                foo.update_lpos(new_foo_lpos)
+            #
             boo.update_lpos(ball_n_pos)
 
         return None
