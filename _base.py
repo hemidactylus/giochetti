6b6a21ef62726c0d2a94a0e244a77faa59cac602
hemidactylus <hemidactylus@users.noreply.github.com>
2026-05-29 19:45:29 +0200

    improved _base

--- _base.py
+++ _base.py
@@ -1,20 +1,23 @@
-# from typing import Literal
+from typing import Literal
 
 import pyglet
 
 from egame.context import Context
 from egame.geometry import Scaler
-from egame.things import PhysicsThing, Thing
+from egame.messenger import Messenger
+from egame.things import Thing
 from egame.type_definitions import FPair
 
 LSIZE = (16, 10)
 SIZE = (1600, 1000)
 
-PLAYER_LSIZE = (0.25, 0.6)
-PLAYER_NOSE_RADIUS = 0.05
+BLOCK_LSIZE = (1, 1)
+SPHERE_LRADIUS = 0.2
+
+BLOCKV_MOD = 3
 
 
-class Player(PhysicsThing):
+class Block(Thing):
     def __init__(
         self,
         *,
@@ -24,62 +27,32 @@ class Player(PhysicsThing):
         body = pyglet.shapes.Rectangle(
             scaler.r_x(lpos[0]),
             scaler.r_y(lpos[1]),
-            scaler.r_x(PLAYER_LSIZE[0]),
-            scaler.r_y(PLAYER_LSIZE[1]),
-            color=(128, 128, 128, 255),
+            scaler.r_x(BLOCK_LSIZE[0]),
+            scaler.r_y(BLOCK_LSIZE[1]),
+            color=(0, 0, 255, 255),
         )
-        nose = pyglet.shapes.Circle(
-            scaler.r_x(lpos[0] + 0.5 * PLAYER_LSIZE[0]),
-            scaler.r_y(lpos[1] + 0.8 * PLAYER_LSIZE[1]),
-            radius=scaler.r_x(PLAYER_NOSE_RADIUS),
-            color=(10, 10, 10, 255),
+        sphere = pyglet.shapes.Circle(
+            scaler.r_x(lpos[0] + 0.5 * BLOCK_LSIZE[0]),
+            scaler.r_y(lpos[1] + 0.5 * BLOCK_LSIZE[1]),
+            radius=scaler.r_x(SPHERE_LRADIUS),
+            color=(255, 10, 10, 255),
         )
-        PhysicsThing.__init__(
+        Thing.__init__(
             self,
             lpos=lpos,
-            lsize=PLAYER_LSIZE,
-            name="player",
+            lsize=BLOCK_LSIZE,
+            name="block",
             sprites={
                 "0": body,
-                "n": nose,
+                "s": sphere,
             },
             sprite_offsets={
                 "0": (0, 0),
-                "n": (0.5 * PLAYER_LSIZE[0], 0.8 * PLAYER_LSIZE[1]),
+                "s": (0.5 * BLOCK_LSIZE[0], 0.5 * BLOCK_LSIZE[1]),
             },
             t0_s=0.0,
-            lv=(0, 0),
-            feels_g=False,
             scaler=scaler,
         )
-
-
-# class Messenger(Thing):
-#     label: pyglet.text.Label
-
-#     def __init__(self, text: str, scaler: Scaler) -> None:
-#         self.label = pyglet.text.Label(
-#             text,
-#             font_name="Times New Roman",
-#             font_size=scaler.r_x(2),
-#             x=0.5 * LSIZE[0],
-#             y=0.5 * LSIZE[1],
-#             anchor_x="center",
-#             anchor_y="center",
-#             color=(40, 255, 60, 128),
-#         )
-#         Thing.__init__(
-#             self,
-#             lpos=(0.5 * LSIZE[0], 0.5 * LSIZE[1]),
-#             lsize=LSIZE,
-#             sprites={"l": self.label},
-#             sprite_offsets={"l": (0, 0)},
-#             t0_s=0.0,
-#             scaler=scaler,
-#         )
-
-#     def set_text(self, text: str) -> None:
-#         self.label.text = text
 
 
 class Scenery(Thing):
@@ -89,7 +62,7 @@ class Scenery(Thing):
             scaler.r_y(0),
             scaler.r_x(LSIZE[0]),
             scaler.r_y(LSIZE[1]),
-            color=(50, 90, 255, 255),
+            color=(90, 20, 150, 255),
         )
         Thing.__init__(
             self,
@@ -108,15 +81,52 @@ class Scenery(Thing):
 
 if __name__ == "__main__":
     ctx0 = Context(size=SIZE, lsize=LSIZE, lg=(0.0, -10.0), time_factor=1.0)
+    ctx0.state["keys_map"] = {}
 
-    player = Player(
-        lpos=(5, 0),
+    block = Block(
+        lpos=(5, 5),
         scaler=ctx0.scaler,
     )
 
     ctx0.push_thing(Scenery(scaler=ctx0.scaler))
-    ctx0.push_thing(player)
-    # messenger = Messenger("Hello", scaler=ctx0.scaler)
-    # ctx0.push_thing(messenger)
+    ctx0.push_thing(block)
+    messenger = Messenger(
+        "CIAO",
+        font_lsize=3,
+        color=(0, 255, 0, 48),
+        context=ctx0,
+        scaler=ctx0.scaler,
+    )
+    ctx0.push_thing(messenger)
+
+    def on_k_p(ctx: Context, symbol: int, modifiers: int) -> Literal[True] | None:
+        ctx.state["keys_map"][symbol] = True
+        return None
+
+    def on_k_r(ctx: Context, symbol: int, modifiers: int) -> Literal[True] | None:
+        ctx.state["keys_map"][symbol] = False
+        return None
+
+    def tk(ctx: Context, dt: float, t: float) -> None:
+        blockv: list[float] = [0, 0]
+        if ctx.state["keys_map"].get(pyglet.window.key.UP):
+            blockv[1] += BLOCKV_MOD
+        if ctx.state["keys_map"].get(pyglet.window.key.DOWN):
+            blockv[1] -= BLOCKV_MOD
+        if ctx.state["keys_map"].get(pyglet.window.key.LEFT):
+            blockv[0] -= BLOCKV_MOD
+        if ctx.state["keys_map"].get(pyglet.window.key.RIGHT):
+            blockv[0] += BLOCKV_MOD
+        if blockv != (0, 0):
+            blockd = (blockv[0] * dt, blockv[1] * dt)
+            new_block_lpos = (
+                block.lpos[0] + blockd[0],
+                block.lpos[1] + blockd[1],
+            )
+            block.update_lpos((new_block_lpos[0], new_block_lpos[1]))
+
+    ctx0.on_key_press(on_k_p)
+    ctx0.on_key_release(on_k_r)
+    ctx0.tick(tk)
 
     ctx0.run()
