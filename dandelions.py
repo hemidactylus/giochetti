7f7a955746178095edd7e98b6a6fe8b775cf6655
hemidactylus <hemidactylus@users.noreply.github.com>
2026-05-31 01:28:17 +0200

    full dandelions

--- dandelions.py
+++ dandelions.py
@@ -16,15 +16,20 @@ BLOCK_LSIZE = (1, 1)
 SPHERE_LRADIUS = 0.2
 
 D_FLOWER_LRADIUS = 0.4
-SPAWN_CHANCE_PER_SECOND = 0.3
+SPAWN_CHANCE_PER_SECOND = 1  # TEMP 0.3
 D_FLOWER_LIFETIME_S = 3
 
 DANDELION_N_PUFFS = 20
 DANDELION_PUFF_LRADIUS = 0.2
 DANDELION_LRADIUS = 0.4
 PUFF_COLOR = (255, 255, 255, 96)
+MAX_DANDELION_COUNT = 8
+D_STICK_WIDTH = 0.1
+D_STICK_HEIGHT = 1
+D_STICK_COLOR = (10, 80, 2, 255)
 
-D_LIFETIME_S = 3  # TEMP
+BLOW_RANGE_X = 1.8
+BLOW_RANGE_Y = 1.5
 
 BLOCKV_MOD = 3
 
@@ -37,11 +42,13 @@ class Puff(Thing):
     p_lvel: FPair
     p_lvel_a: FPair
     p_lvel_omega: FPair
+    xdir: int
 
     def __init__(
         self,
         *,
         lpos: FPair,
+        xdir: int,
         t0_s: float,
         scaler: Scaler,
     ) -> None:
@@ -51,7 +58,8 @@ class Puff(Thing):
             scaler.r_x(DANDELION_PUFF_LRADIUS),
             color=PUFF_COLOR,
         )
-        self.p_lvel = (fluctuate(*PUFF_LVEL[0]), fluctuate(*PUFF_LVEL[1]))
+        self.xdir = xdir
+        self.p_lvel = (xdir * fluctuate(*PUFF_LVEL[0]), fluctuate(*PUFF_LVEL[1]))
         self.p_lvel_a = (fluctuate(*PUFF_LVEL_A[0]), fluctuate(*PUFF_LVEL_A[1]))
         self.p_lvel_omega = (
             fluctuate(*PUFF_LVEL_OMEGA[0]),
@@ -78,7 +86,7 @@ class Puff(Thing):
         # evolve TEMP
         this_p_lvel = (
             self.p_lvel[0] + self.p_lvel_a[0] * math.cos(self.p_lvel_omega[0] * t_s),
-            self.p_lvel[1] + self.p_lvel_a[1] * math.cos(self.p_lvel_omega[1] * t_s),
+            self.p_lvel[1] + self.p_lvel_a[1] * math.sin(self.p_lvel_omega[1] * t_s),
         )
         self.update_lpos(
             (
@@ -90,6 +98,8 @@ class Puff(Thing):
 
 
 class Dandelion(Thing):
+    blown: int
+
     def __init__(
         self,
         *,
@@ -97,6 +107,14 @@ class Dandelion(Thing):
         t0_s: float,
         scaler: Scaler,
     ) -> None:
+        self.blown = 0
+        stick = pyglet.shapes.Rectangle(
+            scaler.r_x(lpos[0] - D_STICK_WIDTH / 2),
+            scaler.r_y(lpos[1] - DANDELION_LRADIUS - D_STICK_HEIGHT),
+            scaler.r_x(D_STICK_WIDTH),
+            scaler.r_y(D_STICK_HEIGHT),
+            color=D_STICK_COLOR,
+        )
         sprite_offsets = {
             f"{ii}": (
                 DANDELION_LRADIUS * math.cos(theta),
@@ -107,6 +125,7 @@ class Dandelion(Thing):
                 for i in range(DANDELION_N_PUFFS)
             )
         }
+        sprite_offsets["s"] = (-D_STICK_WIDTH / 2, -DANDELION_LRADIUS - D_STICK_HEIGHT)
         puff_map: dict[str, Drawable] = {
             spr_k: pyglet.shapes.Circle(
                 scaler.r_x(lpos[0] + spr_off[0]),
@@ -116,6 +135,7 @@ class Dandelion(Thing):
             )
             for spr_k, spr_off in sprite_offsets.items()
         }
+        puff_map["s"] = stick
         Thing.__init__(
             self,
             lpos=lpos,
@@ -131,19 +151,22 @@ class Dandelion(Thing):
         tdies = Thing.dies_on_update(self, ctx, dt, t_s)
         if tdies:
             return tdies
-        if self.t_s > D_LIFETIME_S:
+        if self.blown != 0:
+            ctx.state["dandelion_count"] -= 1
             # spawn puffs
             for pkey, poffset in self.sprite_offsets.items():
-                ctx.push_thing(
-                    Puff(
-                        lpos=(
-                            self.lpos[0] + poffset[0],
-                            self.lpos[1] + poffset[1],
-                        ),
-                        t0_s=t_s,
-                        scaler=self.scaler,
+                if pkey != "s":
+                    ctx.push_thing(
+                        Puff(
+                            lpos=(
+                                self.lpos[0] + poffset[0],
+                                self.lpos[1] + poffset[1],
+                            ),
+                            xdir=self.blown,
+                            t0_s=t_s,
+                            scaler=self.scaler,
+                        )
                     )
-                )
             return True
         return False
 
@@ -156,6 +179,13 @@ class DandelionFlower(Thing):
         t0_s: float,
         scaler: Scaler,
     ) -> None:
+        stick = pyglet.shapes.Rectangle(
+            scaler.r_x(lpos[0] - D_STICK_WIDTH / 2),
+            scaler.r_y(lpos[1] - D_FLOWER_LRADIUS - D_STICK_HEIGHT),
+            scaler.r_x(D_STICK_WIDTH),
+            scaler.r_y(D_STICK_HEIGHT),
+            color=D_STICK_COLOR,
+        )
         body = pyglet.shapes.Circle(
             scaler.r_x(lpos[0]),
             scaler.r_y(lpos[1]),
@@ -169,9 +199,11 @@ class DandelionFlower(Thing):
             name="flower",
             sprites={
                 "0": body,
+                "s": stick,
             },
             sprite_offsets={
                 "0": (0, 0),
+                "s": (-D_STICK_WIDTH / 2, -D_FLOWER_LRADIUS - D_STICK_HEIGHT),
             },
             t0_s=t0_s,
             scaler=scaler,
@@ -257,7 +289,7 @@ class Scenery(Thing):
             scaler.r_y(0),
             scaler.r_x(LSIZE[0]),
             scaler.r_y(LSIZE[1]),
-            color=(90, 20, 150, 255),
+            color=(120, 200, 130, 255),
         )
         Thing.__init__(
             self,
@@ -277,6 +309,7 @@ class Scenery(Thing):
 if __name__ == "__main__":
     ctx0 = Context(size=SIZE, lsize=LSIZE, lg=(0.0, -10.0), time_factor=1.0)
     ctx0.state["keys_map"] = {}
+    ctx0.state["dandelion_count"] = 0
 
     block = Block(
         lpos=(5, 5),
@@ -288,6 +321,39 @@ if __name__ == "__main__":
 
     def on_k_p(ctx: Context, symbol: int, modifiers: int) -> Literal[True] | None:
         ctx.state["keys_map"][symbol] = True
+        if symbol == pyglet.window.key.SPACE:
+            # blow
+            dands: list[Dandelion] = [
+                thg  # type: ignore[misc]
+                for thg in ctx.things
+                if thg.name == "dandelion"
+            ]
+
+            x_range: FPair = (block.lpos[0], block.lpos[0])
+            if block.lvdir > 0:
+                x_range = (block.lpos[0], block.lpos[0] + BLOW_RANGE_X)
+            elif block.lvdir < 0:
+                x_range = (block.lpos[0] - BLOW_RANGE_X, block.lpos[0])
+            y_range = (
+                block.lpos[1] - BLOW_RANGE_Y / 2,
+                block.lpos[1] + BLOW_RANGE_Y / 2,
+            )
+
+            def is_dand_blown(dand: Thing) -> bool:
+                dlpos = dand.lpos
+                return all(
+                    [
+                        dlpos[0] >= x_range[0],
+                        dlpos[0] <= x_range[1],
+                        dlpos[1] >= y_range[0],
+                        dlpos[1] <= y_range[1],
+                    ]
+                )
+
+            for dand in dands:
+                if is_dand_blown(dand):
+                    dand.blown = block.lvdir if block.lvdir != 0 else 1
+
         return None
 
     def on_k_r(ctx: Context, symbol: int, modifiers: int) -> Literal[True] | None:
@@ -296,19 +362,21 @@ if __name__ == "__main__":
 
     def tk(ctx: Context, dt: float, t: float) -> None:
         # spawn
-        chance = rnd()
-        if chance <= SPAWN_CHANCE_PER_SECOND * dt:
-            new_lpos = (
-                0.1 * LSIZE[0] + rnd() * (LSIZE[0] * 0.8),
-                0.1 * LSIZE[1] + rnd() * (LSIZE[1] * 0.8),
-            )
-            ctx.push_thing(
-                DandelionFlower(
-                    lpos=new_lpos,
-                    t0_s=t,
-                    scaler=ctx.scaler,
+        if ctx0.state["dandelion_count"] < MAX_DANDELION_COUNT:
+            chance = rnd()
+            if chance <= SPAWN_CHANCE_PER_SECOND * dt:
+                new_lpos = (
+                    0.1 * LSIZE[0] + rnd() * (LSIZE[0] * 0.8),
+                    0.1 * LSIZE[1] + rnd() * (LSIZE[1] * 0.8),
                 )
-            )
+                ctx0.state["dandelion_count"] += 1
+                ctx.push_thing(
+                    DandelionFlower(
+                        lpos=new_lpos,
+                        t0_s=t,
+                        scaler=ctx.scaler,
+                    )
+                )
         # movement
         blockv: list[float] = [0, 0]
         if ctx.state["keys_map"].get(pyglet.window.key.UP):
