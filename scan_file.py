861dd383873bf65b14c8b83bfdf82e7a7b29d33a
hemidactylus <hemidactylus@users.noreply.github.com>
2026-06-01 23:17:20 +0200

    minor stuff

--- scan_file.py
+++ scan_file.py
@@ -2,8 +2,13 @@ import sys
 
 if __name__ == "__main__":
     fname = sys.argv[1]
+    nbytes: int
+    if len(sys.argv) > 2:
+        nbytes = int(sys.argv[2])
+    else:
+        nbytes = 40
     opened = open(fname, "rb")
-    first_bytes = opened.read(40)
+    first_bytes = opened.read(nbytes)
     print(f"INIZIO DI '{fname}':")
     print("    ", end="")
     for b in first_bytes:
